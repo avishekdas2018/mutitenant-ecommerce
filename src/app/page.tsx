24c3852a62a926949d08ab2c2620ca1261fb8a1b
Avishek Das <dasavishek2013@gmail.com>
2025-04-23 11:32:18 +0530

    theme customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 
 
 export default function Home() {
@@ -8,7 +10,13 @@ export default function Home() {
       <h1 className="text-3xl font-bold">Hello World</h1>
       <p className="text-lg text-rose-500">This is a simple Next.js app with Tailwind CSS and Radix UI.</p>
       Hello World
-      <Button>Hi</Button>
+      <Button variant={"elevated"}>Click Me!</Button>
+      <div>
+        <Input placeholder=""></Input>
+      </div>
+      <div>
+        <Progress value={50} className="w-56" />
+      </div>
     </div>
   );
 }
